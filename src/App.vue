aea706a146da41faf5ead39dfb4610ff6e8e723a
sebast92 <67110384+sebast92@users.noreply.github.com>
2022-11-12 18:27:51 -0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header :user="user"></app-header>
+    <router-view class="container"></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+  data: {
+    boardName: ''
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    box-sizing: border-box;
+    color: #37474f;
+    font-family: 'Lato', Helvetica, Arial, sans-serif;
+    margin: 0 auto;
+    padding: 0;
+    text-align: center;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  .container {
+    padding-bottom: 4rem;
+  }
 </style>
+
